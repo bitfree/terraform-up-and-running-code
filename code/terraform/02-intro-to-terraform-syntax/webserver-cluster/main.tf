db5d671749a2721ac794229f8e6c7ce2ae05d824
gichoi <bitfree@gmail.com>
2020-08-21 10:08:41 +0900

    change region and ami-id

--- code/terraform/02-intro-to-terraform-syntax/webserver-cluster/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/webserver-cluster/main.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.12, <= 0.13"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-northeast-2"
 
   # Allow any 2.x version of the AWS provider
   version = "~> 2.0"
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0c55b159cbfafe1f0"
+  image_id        = "ami-05438a9ce08100b25"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, Seoul" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
