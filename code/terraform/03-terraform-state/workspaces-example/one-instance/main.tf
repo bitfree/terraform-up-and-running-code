649dae853ca8eeca3e1d706b89dd5b6bab90b514
gichoi <bitfree@gmail.com>
2020-08-23 22:12:48 +0900

    I think s3 does not exist in region ap-northeast-2 so can not use the region. use us-east-1

--- code/terraform/03-terraform-state/workspaces-example/one-instance/main.tf
+++ code/terraform/03-terraform-state/workspaces-example/one-instance/main.tf
@@ -1,33 +1,48 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.12, <= 0.13"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 
   # Allow any 2.x version of the AWS provider
   version = "~> 2.0"
 }
 
-terraform {
-  backend "s3" {
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "getfree-tf-test-bucket"
+  acl    = "private"
 
-    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
-    # manually, uncomment and fill in the config below.
-
-    # bucket         = "<YOUR S3 BUCKET>"
-    # key            = "<SOME PATH>/terraform.tfstate"
-    # region         = "us-east-2"
-    # dynamodb_table = "<YOUR DYNAMODB TABLE>"
-    # encrypt        = true
-
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
-}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-
-  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+  versioning {
+    enabled = true
+  }
 
 }
 
+terraform {
+   backend "s3" {
+ 
+     # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+     # manually, uncomment and fill in the config below.
+ 
+ #    bucket         = "terraform-up-and-running-state"
+ #    key            = "terraform.tfstate"
+ #    region         = "us-east-1"
+ #    dynamodb_table = "MY DYNAMODB TABLE"
+ #    encrypt        = true
+ 
+   }
+ }
+
+
+  resource "aws_instance" "example" {
+   ami           = "ami-40d28157"
+ 
+   instance_type = terraform.workspace == "default" ? "t2.micro" : "t2.medium"
+ 
+ }
